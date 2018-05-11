64a9d6bce31df065362b1ee4842a07cd399a8ea7
Zed <clement.francois@epitech.eu>
2018-05-11 17:56:44 +0200

    DbInterface: ADD: InsertionDate and UpdateTime

--- src/app/dbif.ts
+++ src/app/dbif.ts
@@ -1,4 +1,6 @@
 export class DBInterface {
     RawId: string;
     Name: string;
+    InsertionDate: Date;
+    UpdateTime: Date;
 }
--- src/app/tv.ts
+++ src/app/tv.ts
@@ -2,7 +2,5 @@ import { DBInterface } from './dbif';
 
 export class TV extends DBInterface {
     Ip: string;
-    InsertionDate: Date;
-    UpdateTime: Date;
     Composition: string;
 }
