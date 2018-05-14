6a9c276417baaa419e5c09accee22400ae8d484e
Zed <clement.francois@epitech.eu>
2018-05-14 17:51:08 +0200

    Resource: ADD: Get resource from resourceService

--- src/app/resource/resource.component.ts
+++ src/app/resource/resource.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ResourceService } from '../resource.service';
+import { Resource } from '../resource';
+
+/**
+ * Class used to handle the Resource component
+ * @class ResourceComponent
+ */
 @Component({
   selector: 'app-resource',
   templateUrl: './resource.component.html',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourceComponent implements OnInit {
 
-  constructor() { }
+  /** Array of resources */
+  private resources: Resource[] = null;
 
+  /**
+   * Constructor of the Resource component
+   * @constructor
+   * @param resourceService The ResourceService that communicates with the back-end
+   */
+  constructor(private resourceService: ResourceService) { }
+
+  /**
+   * Setup the resources by getting them from the server
+   */
+  setupResources(): void {
+    this.resourceService.getResources()
+        .subscribe(rsrcs => this.resources = rsrcs);
+  }
+
+  /**
+   * Lifecycle of angular
+   */
   ngOnInit() {
+    this.setupResources();
   }
 
 }
