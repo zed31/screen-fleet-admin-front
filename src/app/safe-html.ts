2807eb0742983896c87acbf7676d892a9c59752e
Zed <clement.francois@epitech.eu>
2018-05-16 19:12:01 +0200

    safe-html: ADD: Documentation

--- src/app/safe-html.ts
+++ src/app/safe-html.ts
@@ -2,9 +2,24 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'safeHtml'})
+
+/**
+ * @class SafeHtmlPipe
+ * Class used as a pipe to trust the html
+ */
 export class SafeHtmlPipe implements PipeTransform  {
+
+  /**
+   * @constructor
+   * @param sanitized The sanitized DOM
+   */
   constructor(private sanitized: DomSanitizer) {}
-  transform(value: string) {
+
+  /**
+   * Transform the HTML as trusted
+   * @param value The html value
+   */
+  public transform(value: string) {
     return this.sanitized.bypassSecurityTrustHtml(value);
   }
 }
