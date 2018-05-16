9001ae575faa92d5cdec23fd0a313ace75b15df4
Zed <clement.francois@epitech.eu>
2018-05-16 19:06:39 +0200

    Composition: ADD: Documentation

--- src/app/compositions/compositions.component.ts
+++ src/app/compositions/compositions.component.ts
@@ -8,20 +8,39 @@ import { DBInterface } from '../dbif';
   templateUrl: './compositions.component.html',
   styleUrls: ['./compositions.component.css']
 })
+
+/**
+ * @class CompositionsComponent
+ * Class used to handle the composition component
+ */
 export class CompositionsComponent implements OnInit {
 
+  /** list of composition */
   compositions: Composition[] = null;
+
+  /** The detail composition */
   compositionDetail = 'composition';
 
+  /**
+   * @constructor
+   * @param compositionService The service to handle the composition
+   */
   constructor(private compositionService: CompositionService) { }
 
-  getCompositions(): void {
+  /**
+   * Get all the compositions
+   */
+  public getCompositions(): void {
     this.compositionService
         .getCompositions()
         .subscribe(comps => this.compositions = comps);
   }
 
-  onRemove(data: DBInterface): void {
+  /**
+   * Remove a specific composition
+   * @param data The data being removed
+   */
+  public onRemove(data: DBInterface): void {
     this.compositionService
         .removeComposition(data as Composition)
         .subscribe(_ => this.compositions = this.compositions.filter(d => d.RawId !== data.RawId));
